677fcc754bf9e6160749db48d938117054fc6424
Jonathan Hill <jonathan@jh.codes>
2025-08-04 11:25:14 -0700

    Refactor sand timer element position properties in JS for scalability

--- js/script.js
+++ js/script.js
@@ -7,31 +7,37 @@ const sandTimerContainer = {
 };
 const sandTimerSandTop = {
     element: document.querySelector('.sand-timer-sand-top'),
-    top1: '20%',
-    top2: '100%',
-    top3: '110%',
+    topR1P1: '-82%',
+    topR1P2: '102%',
+    topR2P1: '23%',
+    topR2P2: '0%'
 };
 const sandTimerSandMiddle = {
     element: document.querySelector('.sand-timer-sand-middle'),
-    top1: '20%',
-    top2: '50%',
+    topR1P1: '101%',
+    topR1P2: '50%',
+    topR2P1: '-1%',
+    bottomR1P1: '-1%',
+    bottomR2P1: '101%',
+    bottomR2P2: '50%'
 };
 const sandTimerSandFalling = {
     element: document.querySelector('.sand-timer-sand-falling'),
-    top1: '50%',
-    top2: '56.4%',
-    bottom1: '49%',
-    bottom2: '5%',
-    borderRadiusTop1: '0',
-    borderRadiusTop2: '100rem',
-    borderRadiusBottom1: '100rem',
-    borderRadiusBottom2: '0'
+    topR1P1: '5%',
+    topR1P2: '65%',
+    topR2P1: '30%',
+    topR2P2: '47%',
+    bottomR1P1: '30%',
+    bottomR1P2: '47%',
+    bottomR2P1: '5%',
+    bottomR2P2: '65%'
 };
 const sandTimerSandBottom = {
     element: document.querySelector('.sand-timer-sand-bottom'),
-    top1: '136%',
-    top2: '46%',
-    top3: '41%'
+    bottomR1P1: '23%',
+    bottomR1P2: '0%',
+    bottomR2P1: '-82%',
+    bottomR2P2: '102%'
 };
 const sandTimerTimeRemaining = document.querySelector('.sand-timer-time-remaining');
 const sandTimerTimePassed = document.querySelector('.sand-timer-time-passed');
@@ -40,7 +46,7 @@ const sandTimerTimePassed = document.querySelector('.sand-timer-time-passed');
 // document.addEventListener("DOMContentLoaded", function() {
 // When .sand-timer-container is clicked, start the sand timer
 document.querySelector('.sand-timer-container').addEventListener('click', function() {
-    const timeSet = 5 * 1000;
+    const timeSet = 3 * 1000;
     const totalTime = timeSet + 500; // 500ms for the initial 180 degree rotation
     let totalTimePassed = 0;
     let totalTimeRemaining = totalTime;
@@ -88,41 +94,41 @@ document.querySelector('.sand-timer-container').addEventListener('click', functi
         sandTimerTimePassed.style.transition = 'transform 500ms ease-in-out';
         sandTimerTimePassed.style.transform = `rotate(-${sandTimerContainer.angle}deg)`;
         if (rightSideUp) {
-            sandTimerSandTop.element.style.transition = 'top 0ms linear'; // Find more efficient alternative
-            sandTimerSandTop.element.style.top = '-82%';
+            sandTimerSandTop.element.style.transition = 'none';
+            sandTimerSandTop.element.style.top = sandTimerSandTop.topR1P1;
 
-            sandTimerSandFalling.element.style.bottom = '30%';
+            sandTimerSandFalling.element.style.bottom = 'sandTimerSandFalling.bottomR1P1';
 
             sandTimerSandBottom.element.style.transition = 'bottom 500ms ease';
-            sandTimerSandBottom.element.style.bottom = '23%';
+            sandTimerSandBottom.element.style.bottom = sandTimerSandBottom.bottomR1P1;
 
-            sandTimerSandMiddle.element.style.top = '101%';
-            sandTimerSandMiddle.element.style.bottom = '-1%';
+            sandTimerSandMiddle.element.style.top = sandTimerSandMiddle.topR1P1;
+            sandTimerSandMiddle.element.style.bottom = sandTimerSandMiddle.bottomR1P1;
             setTimeout(() => {
                 sandTimerSandMiddle.element.style.transition = 'top 250ms linear';
-                sandTimerSandMiddle.element.style.top = '50%';
-                sandTimerSandFalling.element.style.transition = 'top 400ms ease-in-out, bottom 250ms linear, border-radius 250ms linear';
-                sandTimerSandFalling.element.style.top = '5%';
-                sandTimerSandFalling.element.style.bottom = '47%';
+                sandTimerSandMiddle.element.style.top = sandTimerSandMiddle.topR1P2;
+                sandTimerSandFalling.element.style.transition = 'top 400ms ease-in-out, bottom 250ms linear';
+                sandTimerSandFalling.element.style.top = sandTimerSandFalling.topR1P1;
+                sandTimerSandFalling.element.style.bottom = sandTimerSandFalling.bottomR1P2;
             }, 250);
             rightSideUp = false;
         } else {
-            sandTimerSandBottom.element.style.transition = 'bottom 0ms linear'; // Find more efficient alternative
-            sandTimerSandBottom.element.style.bottom = '-82%';
+            sandTimerSandBottom.element.style.transition = 'none';
+            sandTimerSandBottom.element.style.bottom = sandTimerSandBottom.bottomR2P1;
 
-            sandTimerSandFalling.element.style.top = '30%';
+            sandTimerSandFalling.element.style.top = sandTimerSandFalling.topR2P1;
 
             sandTimerSandTop.element.style.transition = 'top 500ms ease';
-            sandTimerSandTop.element.style.top = '23%';
-            
-            sandTimerSandMiddle.element.style.top = '-1%';
-            sandTimerSandMiddle.element.style.bottom = '101%';
+            sandTimerSandTop.element.style.top = sandTimerSandTop.topR2P1;
+
+            sandTimerSandMiddle.element.style.top = sandTimerSandMiddle.topR2P1;
+            sandTimerSandMiddle.element.style.bottom = sandTimerSandMiddle.bottomR2P1;
             setTimeout(() => {
                 sandTimerSandMiddle.element.style.transition = 'bottom 250ms linear';
-                sandTimerSandMiddle.element.style.bottom = '50%';
-                sandTimerSandFalling.element.style.transition = 'top 400ms ease-in-out, bottom 250ms linear, border-radius 250ms linear';
-                sandTimerSandFalling.element.style.bottom = '5%';
-                sandTimerSandFalling.element.style.top = '47%';
+                sandTimerSandMiddle.element.style.bottom = sandTimerSandMiddle.bottomR2P2;
+                sandTimerSandFalling.element.style.transition = 'top 400ms ease-in-out, bottom 250ms linear';
+                sandTimerSandFalling.element.style.bottom = sandTimerSandFalling.bottomR2P1;
+                sandTimerSandFalling.element.style.top = sandTimerSandFalling.topR2P2;
             }, 250);
             rightSideUp = true;
         }
@@ -134,45 +140,43 @@ document.querySelector('.sand-timer-container').addEventListener('click', functi
             // step 1
             if (timePassed > 0) {
                 sandTimerSandTop.element.style.transition = `top ${bottomSandDuration}ms linear`;
-                sandTimerSandTop.element.style.top = '102%';
+                sandTimerSandTop.element.style.top = sandTimerSandTop.topR1P2;
 
                 sandTimerSandBottom.element.style.transition = `bottom ${bottomSandDuration}ms linear`;
-                sandTimerSandBottom.element.style.bottom = '0%';
+                sandTimerSandBottom.element.style.bottom = sandTimerSandBottom.bottomR1P2;
             }
 
             // step 2
             if (timeRemaining < flowStartEndDuration + 60) {
                 sandTimerSandMiddle.element.style.transition = `top ${flowStartEndDuration / 2}ms linear`;
-                sandTimerSandMiddle.element.style.top = '101%';
+                sandTimerSandMiddle.element.style.top = sandTimerSandMiddle.topR1P1;
             }
 
             // step 3
             if (timeRemaining < flowStartEndDuration) {
-                sandTimerSandFalling.element.style.transition = `top ${flowStartEndDuration}ms ease, border-radius ${flowStartEndDuration / 3}ms linear`;
-                sandTimerSandFalling.element.style.top = '65%';
-                sandTimerSandFalling.element.style.borderRadius = sandTimerSandFalling.borderRadiusTop2;
+                sandTimerSandFalling.element.style.transition = `top ${flowStartEndDuration}ms ease`;
+                sandTimerSandFalling.element.style.top = sandTimerSandFalling.topR1P2;
             }
         } else {
             // step 1
             if (timePassed > 0) {
                 sandTimerSandBottom.element.style.transition = `bottom ${topSandDuration}ms linear`;
-                sandTimerSandBottom.element.style.bottom = '102%';
+                sandTimerSandBottom.element.style.bottom = sandTimerSandBottom.bottomR2P2;
 
                 sandTimerSandTop.element.style.transition = `top ${topSandDuration}ms linear`;
-                sandTimerSandTop.element.style.top = '0%';
+                sandTimerSandTop.element.style.top = sandTimerSandTop.topR2P2;
             }
 
             // step 2
             if (timeRemaining < flowStartEndDuration + 60) {
                 sandTimerSandMiddle.element.style.transition = `bottom ${flowStartEndDuration / 2}ms linear`;
-                sandTimerSandMiddle.element.style.bottom = '101%';
+                sandTimerSandMiddle.element.style.bottom = sandTimerSandMiddle.bottomR2P1;
             }
             
             // step 3
             if (timeRemaining < flowStartEndDuration) {
-                sandTimerSandFalling.element.style.transition = `bottom ${flowStartEndDuration}ms ease, border-radius ${flowStartEndDuration / 3}ms linear`;
-                sandTimerSandFalling.element.style.bottom = '65%';
-                sandTimerSandFalling.element.style.borderRadius = sandTimerSandFalling.borderRadiusTop2;
+                sandTimerSandFalling.element.style.transition = `bottom ${flowStartEndDuration}ms ease`;
+                sandTimerSandFalling.element.style.bottom = sandTimerSandFalling.bottomR2P2;
             }
         }
     }
